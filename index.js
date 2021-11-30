ca3f006f3bca52328c22078b37586790284f1e43
vaneela khatri <91223701+vaneela@users.noreply.github.com>
2021-11-30 22:44:24 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById("button-1").onclick = function () {
+    let r, g, b, appleColor;
+    r = Math.round(Math.random() * 256);
+    g = Math.round(Math.random() * 256);
+    b = Math.round(Math.random() * 256);
+    appleColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById("Color").style.backgroundColor 
+    = appleColor;
+    document.getElementById("color-text").innerHTML=
+    appleColor;
+
+}
+document.getElementById("button-2").onclick = 
+function(){
+    let color="white";
+    document.getElementById("Color").style.backgroundColor
+    =color;
+    document.getElementById("color-text").innerHTML=
+    "White";
+}
+
