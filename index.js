0818fa77e19aa54beb99860d5f72a88b9eb1230d
GAURAVKR007 <gauravkrthakur07@gmail.com>
2022-10-10 21:56:40 +0530

    Added the Auto-change Button

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
 let audioTurn = new Audio("bing.mpeg");
 
+const heading = document.getElementById("Heading")
+const bar = document.getElementById("bar")
+const autoBtn = document.getElementById("button-3")
+const changeBtn = document.getElementById("button-1")
+
+// changeBtn.addEventListener("click",colorChange())
+
+var set = false;
+var mytimer
 document.getElementById("button-1").onclick = function () {
+   changeColor()
+
+}
+
+function changeColor() {
     let r, g, b, appleColor;
     r = Math.round(Math.random() * 256);
     g = Math.round(Math.random() * 256);
@@ -10,9 +24,20 @@ document.getElementById("button-1").onclick = function () {
     = appleColor;
     document.getElementById("color-text").innerHTML=
     appleColor;
+    bar.style.backgroundColor = appleColor
     audioTurn.play();
-
 }
+
+autoBtn.addEventListener("click",()=>{
+    bar.classList.add('active');
+    if(set===false){
+    mytimer = setInterval(changeColor,7000);
+    set=true;
+    }
+
+})
+
+
 document.getElementById("button-2").onclick = 
 function(){
     audioTurn.play();
@@ -21,5 +46,10 @@ function(){
     =color;
     document.getElementById("color-text").innerHTML=
     "White";
+    bar.style.backgroundColor = `white`
+    heading.style.color = `white`
+    bar.classList.remove('active')
+    clearInterval(mytimer)
+    set = false
 }
 
